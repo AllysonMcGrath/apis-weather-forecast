8a39980d76e42c5da2fa310af0c9c9bbf131e56e
Allyson McGrath <allyson.r.mcgrath@gmail.com>
2021-09-19 11:45:17 -0500

    add display local storage and display forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,112 @@
 var pastSearchList = JSON.parse(localStorage.getItem("cityList")) || [];
 
-
-function pastSearches() {
-    var pastSearch = document.querySelector("#search-input").value;
-    console.log(pastSearch)
-    pastSearchList.push(pastSearch)
-    localStorage.setItem("cityList", JSON.stringify(pastSearchList));
-
-    var pastSearchesEl = document.querySelector(".past-searches-list");
-    pastSearchesEl.innerHTML = "";
+function displayPastSearches() {
+    var pastSearch = JSON.parse(localStorage.getItem("cityList")) || [];
+    var pastSearchesEl = document.querySelector(".past-searches");
 
     for (var i=0; i < pastSearch.length; i++) {
-        var pastSearch = JSON.parse(localStorage.getItem("cityList")) || [];
-        var pastSearchesEl = document.querySelector(".past-searches-list");
-        var pastSearchesLi = document.createElement("li")
+        var pastSearchesLi = document.createElement("button");
+        pastSearchesLi.classList.add("past-button");
+        pastSearchesLi.setAttribute("id", pastSearch[i])
         pastSearchesLi.innerHTML = pastSearch[i];
         pastSearchesEl.appendChild(pastSearchesLi);
 
     };
+}
+
+function updatePastSearches() {
+    var pastSearchesEl = document.querySelector(".past-searches");
+    var pastSearch = document.querySelector("#search-input").value;
+        if (pastSearchList.indexOf(pastSearch) == -1) {
+            pastSearchList.push(pastSearch)
+            localStorage.setItem("cityList", JSON.stringify(pastSearchList));
+        }
+
+    pastSearchesEl.innerHTML = "";
+
+    displayPastSearches();
     
 };
 
+function forecast(response) {
+
+    $(".forecast-title").append("<h3>5-Day Forecast</h3>");
+    var count = 0;
+
+    $(".forecast-day").each(function() {
+        var dayDate = moment().add(count + 1, 'days').format("MM/DD/YY");
+        var htmlDay = "<p class='date'>" + dayDate + "</p>" + 
+        "<img class='icon' id='wicon' src='http://openweathermap.org/img/wn/" + response.daily[count].weather[0].icon + "@2x.png' alt='Weather icon'>" +
+        "<p>Temp: " + response.daily[count].temp.day + " F</p>" +
+        "<p>Wind: " + response.daily[count].wind_speed + " mph</p>" +
+        "<p>Humidity: " + response.daily[count].humidity + "</p>";
+        $(this).append(htmlDay);
+        count++;
+      });
+}
+
 function search() {
 
-var textInput = document.querySelector("#search-input").value;
-
-var locationApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + textInput + "&appid=b579564ec60f2efd0d30f05fbccce8ee";
+    var textInput = document.querySelector("#search-input").value;
+    var locationApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + textInput + "&appid=b579564ec60f2efd0d30f05fbccce8ee";
+    var cityName = "";
 
     fetch(locationApiUrl)
-    .then(function(locationResponse) {
-        return locationResponse.json();
-    })
-    .then(function(locationResponse) {
-        var longitude = locationResponse.coord.lon
-        var latitude = locationResponse.coord.lat
 
-        return fetch(
-            "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&appid=b579564ec60f2efd0d30f05fbccce8ee"
-        )
-    })
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log (response);
-        
-        var resultsTodayEl = document.querySelector(".results-today");
-        resultsTodayEl.innerHTML = "";
-        var resultsToday = document.createElement("p")
-        resultsToday.innerHTML = response.timezone;
-        resultsTodayEl.appendChild(resultsToday);
-        pastSearches();
-    })
+        .then(function(locationResponse) {
+            return locationResponse.json();
+
+        })
+
+        .then(function(locationResponse) {
+            var longitude = locationResponse.coord.lon;
+            var latitude = locationResponse.coord.lat;
+            cityName = locationResponse.name;
+
+            return fetch(
+                "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=b579564ec60f2efd0d30f05fbccce8ee"
+            )
+        })
+
+        .then(function(response) {
+            return response.json();
+        })
+
+        .then(function(response) {
+            console.log (response);
+            clearContent();
+            forecast(response);
+            updatePastSearches();
+            today(response, cityName);
+        })
 };
+
+function today(response, cityName) {
+    var currentDay = moment().format("MM/DD/YY");
+    console.log(currentDay)
+    var htmlToday = "<p class='date-today'>"+ cityName + " (" + currentDay + ")</p>" + 
+                    "<img class='icon' id='wicon' src='http://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png' alt='Weather icon'>" +
+                    "<p>Temp: " + response.current.temp + " F</p>" +
+                    "<p>Wind: " + response.current.wind_speed + "mph</p>" +
+                    "<p>Humidity: " + response.current.humidity + "</p>";
+    $(".today").append(htmlToday);
+              
+}
+
+function clearContent() {
+    $(".forecast-day").each(function() {
+        console.log($(this));
+        $(this).innerHTML = "";
+      });
+    document.querySelector(".today").innerHTML = "";
+    document.querySelector(".forecast-title").innerHTML = "";
+}
+
+displayPastSearches();
+
+$(".past-searches").on("click", "button", function() {
+    var pastSearch = $(this).attr("id");
+    document.querySelector("#search-input").value = pastSearch;
+    document.querySelector(".search-btn").click();
+    console.log(document.querySelector("#search-input"))
+  });
